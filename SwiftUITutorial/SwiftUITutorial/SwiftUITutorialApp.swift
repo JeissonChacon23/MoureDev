b1d6f9e9918102de0454ccd2fa57cfa72305e6ea
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-04 01:12:04 -0500

    Prueba Git

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Jeisson Chacón on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
